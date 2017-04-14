d0f343a785a9eab6f569ecb1809908ad3922f855
Ash Winter <ash_winter@hotmail.co.uk>
2017-04-14 10:58:45 +0100

    Sorted out a couple of reports remaining pre gitignore

---
+++ tests/whatdoesaSponosorGet.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'Demo test localhost' : function (browser) {
+    browser
+      .url("http://localhost:3000")
+      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('body > img', 1000)
+      .pause(1000)
+      .click('#navbar-brand > div > div > ul > li:nth-child(2) > a')
+      .pause(1000)
+      .assert.containsText('#amazingsponsors > h2', 'Our sponsors totally rock! They pay for your learning, beer and swag!')
+      .end();
+  }
+};
